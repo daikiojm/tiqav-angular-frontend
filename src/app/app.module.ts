a96d5d01704064bc830e7f772975fb2b6bfad12c
daikiojm <daiking.ca2@gmail.com>
2017-09-08 20:09:30 +0900

    各種container(page)を追加

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchComponent } from './container/search/search.component';
+import { NewestComponent } from './container/newest/newest.component';
+import { RandomComponent } from './container/random/random.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    NewestComponent,
+    RandomComponent
   ],
   imports: [
     BrowserModule,
