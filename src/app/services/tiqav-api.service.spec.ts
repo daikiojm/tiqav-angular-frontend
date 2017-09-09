adb7d82acd26b80eb0fc115d3af1f6ef17d4baff
daikiojm <daiking.ca2@gmail.com>
2017-09-09 22:04:24 +0900

    tiqav-apiサービスを追加

---
+++ src/app/services/tiqav-api.service.spec.ts
@@ -0,0 +1,15 @@
+import { TestBed, inject } from '@angular/core/testing';
+
+import { TiqavApiService } from './tiqav-api.service';
+
+describe('TiqavApiService', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [TiqavApiService]
+    });
+  });
+
+  it('should be created', inject([TiqavApiService], (service: TiqavApiService) => {
+    expect(service).toBeTruthy();
+  }));
+});
