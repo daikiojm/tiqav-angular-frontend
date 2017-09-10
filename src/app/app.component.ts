b00b7f1e15f8ec383457aaed801ffc559788af99
daikiojm <daiking.ca2@gmail.com>
2017-09-10 18:26:22 +0900

    routerと連携するtabの実装

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  private routeLinks: any[];
+  private activeLinkIndex = 0;
+  private currentRoute = '';
+
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {
+    this.routeLinks = [
+      { label: 'Search', link: 'search' },
+      { label: 'Newest', link: 'newest' },
+      { label: 'Random', link: 'random' }
+    ];
+    router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.currentRoute = event.url.slice(1);
+        console.log(this.currentRoute);
+        this.routeLinks.forEach((elm, index) => {
+          if (elm.link === this.currentRoute) {
+            this.activeLinkIndex = index;
+          }
+        });
+      }
+    });
+  }
 }
