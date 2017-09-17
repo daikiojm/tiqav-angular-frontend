74c7adba1f889c4788219554006c3d636c5d484b
daikiojm <daiking.ca2@gmail.com>
2017-09-17 18:05:39 +0900

    関連画像の実装(途中)

--- src/app/shared/images-related/images-related.component.ts
+++ src/app/shared/images-related/images-related.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 
 import { Image } from './../../model/image';
+import { TiqavApiService } from './../../services/tiqav-api.service';
 
 @Component({
   selector: 'app-images-related',
@@ -8,11 +9,36 @@ import { Image } from './../../model/image';
   styleUrls: ['./images-related.component.css']
 })
 export class ImagesRelatedComponent implements OnInit {
+  results: Image[] = [];
+  relatedImages: Image[] = [];
 
   @Input() image: Image;
-  constructor() { }
+  constructor(
+    private tiqavApiService: TiqavApiService
+  ) { }
 
   ngOnInit() {
+    // Relatedとしてるけど実際はランダムに取ってくる...
+    this.tiqavApiService.getRandom()
+    .subscribe(
+      data => this.results = data,
+      err => console.log(err),
+      () => {
+        this.relatedImages = this.getRandomN(5);
+      }
+    );
   }
 
+  getRandomN(n: number): Image[] {
+    const result = [];
+    const len = this.results.length;
+    for (let i = 0; i < n; i++) {
+      result.push(this.results[Math.floor(Math.random() * len)]);
+    }
+    return result;
+  }
+
+  getThumbnail(id: string, ext: string): string {
+    return this.tiqavApiService.getThumbnailUrl(id, ext);
+  }
 }
